f045216cd76435ca9b38e72472d7bc40287d4f4e
JaeheeP <niaro4859@gmail.com>
2021-10-02 03:38:40 +0900

    Eddit Select_Stars

--- src/Select_Stars.m
+++ src/Select_Stars.m
@@ -1,7 +1,7 @@
 function [outputCatalog] = Select_Stars(inputCatalog,inputParam)
 %Select_Star: 그림으로 표현할 영역 내에 존재하는 별을 추려내는 함수.
 %   자세한 설명 위치
-% FoV들의 대각을 구하는 식: 피타고라스로 했기 때문에 수정이 필요함.
+% FoV들의 대각을 구하는 식: 피타고라스로 가정.
     Radius = sqrt(inputParam.FoVx^2 + inputParam.FoVy^2)/2;
     CenterVector = Transform.Celestial2Cartesian(inputParam.RA,inputParam.DEC);  
     
@@ -10,8 +10,11 @@ function [outputCatalog] = Select_Stars(inputCatalog,inputParam)
     for i=1:size(inputCatalog,1)
         RA_Star = inputCatalog(i,2);
         DEC_Star = inputCatalog(i,3);
-        StarVector = Transform.Celestial2Cartesian(RA_Star,DEC_Star);
-        if (dot(CenterVector,StarVector) >= cosd(Radius))
+        StarVector = Transform.Celestial2Cartesian(RA_Star,DEC_Star);     
+        Check = dot(CenterVector,StarVector);
+
+        if (Check >= cosd(Radius))
+%             fprintf("%f %f %d\n",Check, cosd(Radius), i);
             outputCatalog(n,:) = inputCatalog(i,:);
             n = n+1;
         end
--- src/Transform.m
+++ src/Transform.m
@@ -14,18 +14,22 @@ classdef Transform
             W = sind(DEC);
             Vector = [U,V,W];
         end
+
+        function [Vector] = Celestial2Cartesian2(RA, DEC)
+            RA = RA * pi/180;
+            DEC = DEC * pi/180;
+            U = cos(RA)*cos(DEC);
+            V = sin(RA)*cos(DEC);
+            W = sin(DEC);
+            Vector = [U,V,W];
+        end
         
         % Inner product of R which points center of the screen and S which points star 
         % from the origin
         function Output = RdotS(phi_Center, theta_Center, phi_Star, theta_Star)
             Output =  sind(phi_Star)*cosd(theta_Star)*sind(phi_Center)*cosd(theta_Center) ...
             + sind(phi_Star)*sind(theta_Star)*sind(phi_Center)*sind(theta_Center) ...
-            + cosd(phi_Star)*cosd(phi_Center);
-            % Center position (RA, DEC) and FoV angle
-%             RA_Center = inputParam.RA;
-%             DEC_Center = inputParam.DEC;
-%             FoV = inputParam.FoV*pi/180.0;
-%             Angle = acos( 1.0-FoV/(2.0*pi) ); 
+            + cosd(phi_Star)*cosd(phi_Center);            
         end
         
     end
