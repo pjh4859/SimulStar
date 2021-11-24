b5e364be00b601eb4cea1c569fed1eaf6445257d
jaehee <niaro4859@gmail.com>
2021-11-24 23:47:43 +0900

    Add Labeling

--- src/StarID/RegionLabeling.m
+++ src/StarID/RegionLabeling.m
@@ -1,7 +1,24 @@
-function [outputArg1,outputArg2] = RegionLabeling(StarMat, PixX, PixY)
-%REGIONLABELING 이 함수의 요약 설명 위치
-%   자세한 설명 위치
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [LabeledMatrix] = RegionLabeling(StarMat, PixX, PixY, Threshold)
+%REGIONLABELING 
+%  별 이미지 매트릭스에서 스레스홀드 이상의 밝기를 가진 별들만 라벨링해서 새로운 배열을 만든다.
+
+% 넘버링될 별 라벨 번호.
+StarNum = 1;
+% padding을 위해 들어온 매트릭스 크기보다 가로, 세로 2씩 더 큰 zero 배열을 만든다.
+tempMatrix = zeros(PixY+2, PixX+2);
+% 위에서 만든 zero 배열 가운데에 원래 매트릭스를 넣는다.
+tempMatrix(2:PixY + 2 - 1, 2:PixX + 2 - 1) = StarMat;
+
+for i = 1:PixY
+    for j = 1:PixX
+        if tempMatrix(1+i, 1+j) >= Threshold
+            if tempMatrix(1+i, 1+j)
+
+            end
+
+        end
+
+    end
 end
 
+end
