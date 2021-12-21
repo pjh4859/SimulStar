d1e57ab021a54cb0ffe47f1015a5dd2dfb7f5817
JaeheePaRK <niaro4859@gmail.com>
2021-12-21 17:37:50 +0900

    Add TrackingMode

---
+++ src/StarMap/ReadPosiFile.m
@@ -0,0 +1,14 @@
+function [EOF] = ReadPosiFile(filepath)
+%READPOSIFILE 이 함수의 요약 설명 위치
+%   자세한 설명 위치
+arguments
+    filepath = '../BSCatalog/BSCatalog.txt';
+end
+
+if exist()
+   mkdir(filepath); 
+end
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
